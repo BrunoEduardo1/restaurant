9156bafe98aa1597b869d665d9f57015c64407fe
BrunoEduardo1 <hueteste2@gmail.com>
2018-06-20 06:17:26 -0300

    algumas mudanças de estilo na pagina inicia(linhas subtitulo e correção do erro de js não págins que não têm o slider

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 $(function () {
 	 //mascara para o numero da matricula
 	 console.log('Js');
-	 //deve ser cahmado primeiro
-	 $('.slider').bxSlider({
-		speed: 500,
-		tickerHover:true,
-		auto:true,
-		pager:false,
-		controls: true,
-		nextText: 'Proximo',
-		prevText:'Anterior',
-		infiniteLoop: true,
-		autoHover: true,
-		pause: 4000,
-		stopAutoOnClick: true,
-		ticker: false
-	});
-	 $('.dataNas').mask('00/00/0000');
-
+	  $('.dataNas').mask('00/00/0000');
+	 //A pagina tem a classe slider?
+	 if ($(this).has('.slider').length) {
+		 $('.slider').bxSlider({
+			speed: 500,
+			tickerHover:true,
+			auto:true,
+			pager:false,
+			controls: true,
+			nextText: 'Proximo',
+			prevText:'Anterior',
+			infiniteLoop: true,
+			autoHover: true,
+			pause: 4000,
+			stopAutoOnClick: true,
+			ticker: false
+		});
+	}
 
 
 });
